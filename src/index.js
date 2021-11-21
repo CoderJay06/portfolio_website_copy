b87304a5529a943f865993bba6ad831fc1af571b
Jay Cruz <49811999+CoderJay06@users.noreply.github.com>
2021-11-21 18:16:33 -0500

    Add in all links

--- src/index.js
+++ src/index.js
@@ -16,15 +16,8 @@ async function fetchBlogs() {
 fetchBlogs().then((data) => {
   const { items: blogs } = data;
 
-  /*
-    title: "Optimize Fibonacci with Dynamic Programming", pubDate: "2021-09-17 00:38:02", link: "https://javascript.plainenglish.io/optimize-fibonacci-with-dynamic-programming-2b31e72c5e03?source=rss-530d9586f2f------2", guid: "https://medium.com/p/2b31e72c5e03", author: "Jay Cruz"…}
-    title: "Optimize Fibonacci with Dynamic Programming"
-    pubDate: "2021-09-17 00:38:02"
-    link: "https://javascript.plainenglish.io/optimize-fibonacci-with-dynamic-programming-2b31e72c5e03?source=rss-530d9586f2f------2"
-    guid: "https://medium.com/p/2b31e72c5e03"
-    author: "Jay Cruz"
-    thumbnail: "https://cdn-images-1.medium.com/max/2600/0*-AcDV99KKwUE9Ths"
-  */
+  // render each blog to the page
+  blogsList.innerHTML = getBlogs();
 
   function getBlogs() {
     return blogs
@@ -44,9 +37,6 @@ fetchBlogs().then((data) => {
       })
       .join("");
   }
-
-  // iterate blogs and render each to page
-  blogsList.innerHTML = getBlogs();
 });
 
 // render the blogs
